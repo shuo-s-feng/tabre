4608c25cff1bf81d10d35294584991fb883ab574
shuo-s-feng <shuo.s.feng@gmail.com>
2025-09-29 10:44:17 +0800

    fix: Fix broken urls to API definition jsons

--- src/constants/presaved-definitions.ts
+++ src/constants/presaved-definitions.ts
@@ -1,80 +1,80 @@
 export const TYPEAHEAD_DEFINITIONS = [
   {
     label: "Typeahead General",
-    path: "/linkedin.com/single-step/typeahead-general.json",
+    path: "linkedin.com/single-step/typeahead-general.json",
   },
   {
     label: "Typeahead Locations",
-    path: "/linkedin.com/single-step/typeahead-locations.json",
+    path: "linkedin.com/single-step/typeahead-locations.json",
   },
   {
     label: "Typeahead Companies",
-    path: "/linkedin.com/single-step/typeahead-companies.json",
+    path: "linkedin.com/single-step/typeahead-companies.json",
   },
   {
     label: "Typeahead Schools",
-    path: "/linkedin.com/single-step/typeahead-schools.json",
+    path: "linkedin.com/single-step/typeahead-schools.json",
   },
   {
     label: "Typeahead Industries",
-    path: "/linkedin.com/single-step/typeahead-industries.json",
+    path: "linkedin.com/single-step/typeahead-industries.json",
   },
   {
     label: "Typeahead People",
-    path: "/linkedin.com/single-step/typeahead-people.json",
+    path: "linkedin.com/single-step/typeahead-people.json",
   },
   {
     label: "Typeahead Service Categories",
-    path: "/linkedin.com/single-step/typeahead-service-categories.json",
+    path: "linkedin.com/single-step/typeahead-service-categories.json",
   },
   {
     label: "Typeahead Functions",
-    path: "/linkedin.com/single-step/typeahead-functions.json",
+    path: "linkedin.com/single-step/typeahead-functions.json",
   },
   {
     label: "Typeahead Titles",
-    path: "/linkedin.com/single-step/typeahead-titles.json",
+    path: "linkedin.com/single-step/typeahead-titles.json",
   },
 ];
 
 export const SEARCH_DEFINITIONS = [
   {
     label: "Search Companies",
-    path: "/linkedin.com/single-step/search-companies.json",
+    path: "linkedin.com/single-step/search-companies.json",
   },
   {
     label: "Search Schools",
-    path: "/linkedin.com/single-step/search-schools.json",
+    path: "linkedin.com/single-step/search-schools.json",
   },
 ];
 
 export const GET_DETAILS_DEFINITIONS = [
   {
     label: "Get Company Details",
-    path: "/linkedin.com/single-step/get-company-details.json",
+    path: "linkedin.com/single-step/get-company-details.json",
   },
   {
     label: "Get User Profile",
-    path: "/linkedin.com/single-step/get-user-profile.json",
+    path: "linkedin.com/single-step/get-user-profile.json",
   },
   {
     label: "Get User Profile (Legacy)",
-    path: "/linkedin.com/single-step/get-user-profile-legacy.json",
+    path: "linkedin.com/single-step/get-user-profile-legacy.json",
   },
 ];
 
 export const MULTIPLE_STEP_DEFINITIONS = [
   {
     label: "Search Jobs",
-    path: "/linkedin.com/multiple-steps/search-jobs.json",
+    path: "linkedin.com/multiple-steps/search-jobs.json",
   },
   {
     label: "Get Company Details",
-    path: "/linkedin.com/multiple-steps/get-company-details.json",
+    path: "linkedin.com/multiple-steps/get-company-details.json",
   },
   {
     label: "Search People",
-    path: "/linkedin.com/multiple-steps/search-people.json",
+    path: "linkedin.com/multiple-steps/search-people.json",
   },
 ];
 
